5fa61574c38ee6454f734f695cef8a99fe681a65
chylvano <chylvanolandburg@gmail.com>
2023-04-24 14:38:52 +0200

    admin glitche fixed

--- resources/views/category/showproductsbycategory.blade.php
+++ resources/views/category/showproductsbycategory.blade.php
@@ -53,19 +53,21 @@
                                 <h6 class="mb-3 text-success">In stock</h6>
                             @endif
                             <input type="hidden" name="id" id="id" value="{{$product->id}}"/>
-                            <div class="button-box">
-                                <a href="{{ url('/product/' . 'details/' . $product->id . "/" . $product->category_id) }}" class="btn btn-dark btn-sm">See product</a>
+                            @if(Auth::check() && Auth::user()->admin == "1")
+                                <div class="button-box">
+                            @endif  
+                            <a href="{{ url('/product/' . 'details/' . $product->id . "/" . $product->category_id) }}" class="btn btn-dark btn-sm">See product</a>
+                            @if(Auth::check() && Auth::user()->admin == "1")
                                 <a href="{{ url('/product/edit/' . $product->id) }}" class="btn btn-success btn-sm">Edit</a>
                                 <a href="{{ url('/product/delete/' . $product->id) }}" class="btn btn-danger btn-sm">Delete</a>
-                            </div>
-                            @if(Auth::check() && Auth::user()->admin == "1")
-                                <hr>                            
+                                </div>
+                                <hr>                     
                                 <form action="{{ url('product/storeStockOfProduct/' .$product->id) }}" method="post">
                                 <input type="hidden" name="id" id="id" value="{{$product->id}}"/>
                                 <form action="{{ url('cart/index/' . $product->id) }}" method="post">
                                     {!! csrf_field() !!}
                                     <input type="hidden" name="id" id="id" value="{{$product->id}}"/>
-                                    <label>Stock of {{$product->name}}</label></br>
+                                    <label>Stock:</label></br>
                                     <input type="text" name="stock" id="stock" value="{{$product->stock}}" class="form-control"><br>
                                     <input type="submit" value="Update stock" class="btn btn-success btn-sm"><br>
                                 </form>
@@ -77,9 +79,11 @@
                             <form action="{{ url('register') }}">
                             @endif
                                 {!! csrf_field() !!}
-                                <input type="number" value="1" min="1" class="form control" name="amount"><br>
                                 <br>
-                                <input type="submit" value="Add to cart" class="btn btn-dark"></br>
+                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                <input type="number" name="quantity" value="1" min="1"><br>
+                                <br>
+                                <button type="submit" class="btn btn-dark">Add to cart </button><br>
                             </form>
                         </div>
                     </div>
